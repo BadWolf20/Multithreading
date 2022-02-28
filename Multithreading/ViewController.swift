258655ceb82ba0b3f6750feb64a6d9ed531b917f
BadWolf20 <Roman.Pichugin20@gmail.com>
2022-02-28 13:00:42 +0700

    Storyboard setup

--- Multithreading/ViewController.swift
+++ Multithreading/ViewController.swift
@@ -9,6 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passwordGenButton: UIButton!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var backgroundButton: UIButton!
+
+
+    @IBAction func passwordGenAction(_ sender: UIButton) {
+    }
+
+    @IBAction func backgroundAction(_ sender: UIButton) {
+    }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
